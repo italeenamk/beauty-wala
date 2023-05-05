9621190e9605226676964a86a0a7308c4c0fe938
italeenamk <italeenamk@gmail.com>
2023-05-05 20:09:34 +0500

    creating services-page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../layout/Navbar";
 import Mainslider from "../layout/Mainslider";
+import Services from "../layout/Services";
 
 
 const Home = () => {
   return(
       <>
           <Mainslider/>
+          <Services/>
       </>
   )
 }
---
+++ src/pages/layout/Services.jsx
@@ -0,0 +1,11 @@
+import '../style/services.css';
+
+
+const Services = () => {
+  return(
+      <>
+      </>
+  )
+}
+
+export default Services;
