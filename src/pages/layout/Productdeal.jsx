7287767a6df084d534826f9e06e8f97fb2c8b8fa
italeenamk <italeenamk@gmail.com>
2023-05-06 16:06:01 +0500

    product-deal..

--- src/pages/layout/Productdeal.jsx
+++ src/pages/layout/Productdeal.jsx
@@ -3,6 +3,10 @@ import '../style/productdeal.css';
 const Productdeal = () => {
   return(
       <>
+          <section id="today-deal-main-content">
+              <h1>Deal Of The Days</h1>
+              <div className="today-deal-product-card-content"></div>
+          </section>
       </>
   )
 }
