6707329ad499f879ffd799ae7df8b5dc6f14615b
italeenamk <italeenamk@gmail.com>
2023-05-11 20:11:36 +0500

    slider icon

--- src/pages/layout/Arrivalsproduct.jsx
+++ src/pages/layout/Arrivalsproduct.jsx
@@ -341,12 +341,12 @@ class Arrivalsproduct extends React.Component {
                     </div>
                     <a className="carousel-control-prev" href="#carouselExampleControls" role="button"
                        data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-prev-icon" aria-hidden="true">mm,</span>
                         <span className="sr-only">Previous</span>
                     </a>
                     <a className="carousel-control-next" href="#carouselExampleControls" role="button"
                        data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-next-icon" aria-hidden="true">jkj</span>
                         <span className="sr-only">Next</span>
                     </a>
                 </div>
